fdfda186131e99476f33577886ae912b26ef22de
Ernesto Martinez <ernesto_perez@cuaieed.unam.mx>
2021-06-27 20:42:51 -0500

    Seccion de proyectos agregada

--- components/Projects.js
+++ components/Projects.js
@@ -5,24 +5,7 @@ import Project from 'components/projects/Project';
 
 const Projects = () => {
 	return (
-		<section className="
-			mx-5
-			pt-10
-			pb-3
-			border-t
-			border-gray
-
-			md:grid
-			md:grid-cols-12
-			md:gap-6
-			md:m-0
-			md:pb-10
-
-			xl:inline-block
-			xl:border-none
-			xl:w-1/4
-			xl:float-left
-		">
+		<section className="projects-container">
 			<Project />
 			<Project />
 			<Project />
